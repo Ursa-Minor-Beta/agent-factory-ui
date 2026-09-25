0d4562450bbf7de6f84131509d629f650811215b
Dima-d <9792910@gmail.com>
2026-09-25 11:58:22 +0300

    fix types import

--- src/pages/Collections/index.ts
+++ src/pages/Collections/index.ts
@@ -3,5 +3,4 @@ export { CollectionRecordsPage } from './CollectionRecords';
 export { CollectionModal } from './CollectionModal';
 export { CollectionDeleteModal } from './CollectionDeleteModal';
 export { RecordModal } from './RecordModal';
-export { RecordViewModal } from './RecordViewModal';
 export { RecordDeleteModal } from './RecordDeleteModal';
